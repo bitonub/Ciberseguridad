5468a41f4d6212e44b5e79d8f3caa5def7ea6169
bitonub <gilberto.moralesm@uanl.edu.mx>
2025-09-23 23:30:12 -0600

    Add files via upload

---
+++ Evidencia 10/Evidencia10_validar_archivos.ps1
@@ -0,0 +1,35 @@
+function Validar-Archivo {
+    param ([string]$Ruta)
+
+    
+    $fecha = Get-Date -Format "ddMMyyyy"
+    $reporte = "$PSScriptRoot\Evidencia10_reporte_$fecha.txt"
+
+    try {
+        if (Test-Path $Ruta) {
+            $contenido = Get-Content $Ruta -ErrorAction Stop
+            $mensaje = "Archivo encontrado y accesible: $Ruta"
+        } else {
+            throw 
+        }
+    }
+    catch {
+        $mensaje = "Error en $Ruta : $_"
+    }
+    finally {
+        
+        Write-Host "Validación finalizada para: $Ruta"
+
+        
+        Add-Content -Path $reporte -Value "$(Get-Date -Format 'HH:mm:ss') - $mensaje"
+    }
+
+    return $mensaje
+}
+
+
+# Archivo inexistente
+Validar-Archivo -Ruta "C:\archivo_inexistente.txt"
+
+# Archivo en Escritorio
+Validar-Archivo -Ruta "$env:USERPROFILE\Desktop\archivo.txt"
